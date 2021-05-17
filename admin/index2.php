0484498a1eda35485de5a75e7156f53efca7ac89
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-05-17 15:46:15 +0800

    index2

--- admin/index2.php
+++ admin/index2.php
@@ -112,28 +112,6 @@ if($_POST["ngapain"] == 'insert') {
 }
  
  
- 
-$posts = $conn->query("SELECT * FROM Post");
-?>
-<a href="insert.php">Buat artikel</a>
-<?php
-while($post = $posts->fetch_object()) {
-?>
-   <div class="card">
-       <a href="post.php?id=<?php echo $post->id ?>"><h2><?php echo $post->title; ?></h2></a>
-       <h5><?php echo $post->createdAt; ?></h5>
-       <div class="fakeimg" style="height:200px;">Image</div>
-       <a href="update.php?id=<?php echo $post->id ?>">Edit</a>
-       <a href="index.php?delete=artikel&id=<?php echo $post->id ?>">delete</a>
-   </div>
-   <?php
-}
-?>
-
-<?php include "../footer.php" ?>
-
-
-
 
 
  
