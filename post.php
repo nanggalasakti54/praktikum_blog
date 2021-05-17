2285032042db382117b1240744888659d81a5d14
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-05-17 15:40:56 +0800

    edit index admin

--- post.php
+++ post.php
@@ -1,19 +1,16 @@
-<?php include "header.php" ?>
+<?php
+include "header.php";
+ 
+$id = $_GET["id"];
+$result = $conn->query("SELECT * FROM Post WHERE id=$id");
+$post = $result->fetch_object();
+ 
+?>
 <div class="card">
-   <h2>TITLE HEADING</h2>
-   <h5>Title description, Dec 7, 2017</h5>
+   <h2><?php echo $post->title ?></h2>
+   <h5><?php echo $post->createdAt ?></h5>
    <div class="fakeimg" style="height:200px;">Image</div>
-   <p>Magna irure laborum quis cupidatat qui ad ut amet laboris culpa laboris esse id ut. Pariatur aliquip reprehenderit amet laboris fugiat. Consectetur amet labore ipsum ullamco dolore deserunt. Et tempor ut enim labore magna sit. Aliquip commodo duis veniam magna id adipisicing pariatur Lorem nostrud proident sit aliqua. Sunt excepteur aliquip consequat do ipsum velit ut exercitation in id nostrud fugiat fugiat enim.
- 
-Voluptate commodo et qui nisi. Ipsum reprehenderit dolore nulla ex ad cupidatat reprehenderit nostrud enim quis eu incididunt culpa aute. Cillum consequat sint ad qui irure elit elit enim consequat mollit laborum. Quis consectetur proident dolore proident cupidatat dolore. Sint eiusmod minim esse dolor duis Lorem magna nulla est magna anim laborum. Ullamco consequat nulla cupidatat in sunt nostrud ullamco eu in commodo ipsum. Voluptate voluptate dolore ad labore cupidatat amet voluptate irure esse veniam est reprehenderit.
- 
-Minim nulla cupidatat cupidatat id aute adipisicing ipsum. Anim deserunt commodo duis nulla sit dolore consequat Lorem duis sint et nisi ipsum labore. Minim nulla sunt cillum excepteur cupidatat ipsum ad qui velit ut consectetur. Dolor minim ex nulla laborum non aliquip consequat proident anim nostrud. Laboris velit eu exercitation ex reprehenderit excepteur ullamco nulla incididunt eiusmod irure pariatur. Nisi sunt adipisicing ullamco id laboris ut eu id nostrud laboris occaecat.
- 
-Sint laborum tempor cupidatat enim nulla tempor minim fugiat magna culpa aliquip amet. Veniam aliquip voluptate amet commodo elit proident consectetur qui adipisicing commodo ea. Lorem veniam fugiat tempor qui proident nulla eiusmod eu aliquip amet. Sint ullamco culpa ea eiusmod incididunt est.
- 
-Est occaecat pariatur et sunt dolore est sunt amet esse voluptate exercitation. Nostrud ullamco in exercitation eiusmod fugiat exercitation labore proident Lorem duis dolor. Labore proident in enim fugiat aliquip occaecat consequat dolore eiusmod est occaecat. Aliquip ad ullamco magna anim qui ullamco. Enim ut consequat incididunt incididunt consectetur proident. Eu non elit deserunt labore.
- 
-Et enim eiusmod voluptate nostrud adipisicing Lorem dolor consectetur anim proident et labore. Ipsum aliquip id anim ullamco deserunt elit labore in et cillum eiusmod dolore. Proident deserunt ipsum culpa excepteur elit Lorem voluptate nulla irure cupidatat ipsum. Incididunt voluptate cillum sit aliqua officia pariatur in aliquip officia nisi irure labore Lorem. Pariatur adipisicing cillum ad dolor ad dolore.</p>
+   <p><?php echo $post->content ?></p>
 </div>
  
 <?php include "footer.php" ?>
