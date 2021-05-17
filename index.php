b27125996ffaac0c864034d95ae7fe58b2be90aa
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-05-17 16:22:48 +0800

    ulang index

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
    </div>
    <div class="card">
      <h3>Follow Me on Twitter</h3>
-     <p>@nanggala_sakti</p>
+     <p>@humaprathama</p>
    </div>
  </div>
 </div>
@@ -63,43 +63,6 @@
 <div class="footer">
  <h2>Footer</h2>
 </div>
-
-<?php
-include "header.php";
- 
-$posts = $conn->query("SELECT * FROM Post");
- 
-while($post = $posts->fetch_object()) {
- ?>
-   <div class="card">
-       <a href="post.php"><h2><?php echo $post->title; ?></h2></a>
-       <h5><?php echo $post->createdAt; ?></h5>
-       <div class="fakeimg" style="height:200px;">Image</div>
-   </div>
-   <?php
-}
-?>
-<?php include "footer.php" ?>
-
-<?php
-include "header.php";
- 
-$posts = $conn->query("SELECT * FROM Post");
- 
-while($post = $posts->fetch_object()) {
- ?>
-   <div class="card">
-       <a href="post.php?id=<?php echo $post->id ?>"><h2><?php echo $post->title; ?></h2></a>
-       <h5><?php echo $post->createdAt; ?></h5>
-       <div class="fakeimg" style="height:200px;">Image</div>
-   </div>
-   <?php
-}
-?>
-<?php include "footer.php" ?>
-
-
-
  
 </body>
 </html>
