5784a9e31b593f704dd5ec7a326ae5697c72f280
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-05-17 23:29:57 +0800

    edit index, header dan footer

--- index.php
+++ index.php
@@ -5,20 +5,18 @@
 </head>
 <body>
  
-<div class="header">
- <h1>My Blog</h1>
- <p>Mari Berbagi Cerita</p>
-</div>
-
 <?php include "header.php" ?>
-
+ 
 <!-- Menampikan kolom sebelah kiri -->
+
 <div class="row">
  <div class="leftcolumn">
    <div class="card">
      <a href="post.php"><h2>TITLE HEADING</h2></a>
      <h5>Title description, Dec 7, 2017</h5>
-     <div class="fakeimg" style="height:200px;">Image</div>
+     <div class="fakeimg" style="height: 200px;">
+     
+     </div>
    </div>
    <div class="card">
      <a href="post.php"><h2>TITLE HEADING</h2></a>
@@ -41,30 +39,7 @@
      <div class="fakeimg" style="height:200px;">Image</div>
    </div>
  </div>
- 
- <!-- Menampikan kolom sebelah kanan -->
- <div class="rightcolumn">
-   <div class="card">
-     <h2>About Me</h2>
-     <div class="fakeimg" style="height:100px;">Image</div>
-     <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-   </div>
-   <div class="card">
-     <h3>Popular Post</h3>
-     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
-     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
-     <a href="post.php"><div class="fakeimg"><p>Image</p></div></a>
-   </div>
-   <div class="card">
-     <h3>Follow Me on Twitter</h3>
-     <p>@guntur</p>
-   </div>
- </div>
-</div>
- 
-<div class="footer">
- <h2>Footer</h2>
-</div>
+
 
 <?php include "footer.php" ?>
  
